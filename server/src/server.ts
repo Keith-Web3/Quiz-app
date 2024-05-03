9480e5e4006529f76d2c0187952ba08f210b0fe7
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-05-03 17:24:33 +0100

    feat: setup mondb server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,10 @@
+import mongoose from 'mongoose'
 import app from './app.js'
 
 app.listen(process.env.PORT, () => {
   console.log(`App is running on port ${process.env.PORT}`)
 })
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log('connected to db successfully'))
