20d489ff05bac2a6ce87d8054ae122eebba310b8
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-05-09 21:00:41 +0100

    feat: added duplicate field error handler

--- server/src/controllers/errorController.ts
+++ server/src/controllers/errorController.ts
@@ -1,13 +1,20 @@
 import { NextFunction, Request, Response } from 'express'
 import AppError from '../utils/appError.js'
+import { MongooseError } from 'mongoose'
 
 export default async function (
-  err: AppError,
+  err: any,
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  console.log(err)
+  if (err.name === 'MongoServerError' && err.code === 11000) {
+    const duplicateField = Object.keys(err.errorResponse.keyValue)[0]
+    return res.status(401).json({
+      status: 'failed',
+      message: `Duplicate ${duplicateField}. Please use another value!`,
+    })
+  }
   if (!err.isOperational) {
     return res.status(500).json({
       message: 'Something went really wrong',
