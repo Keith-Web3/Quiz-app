912865a10211344c054c48741365ff956cc152d9
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-05-09 07:10:35 +0100

    fix: added '/' route to middleware matcher

--- client/middleware.ts
+++ client/middleware.ts
@@ -2,19 +2,21 @@ import { NextResponse, type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get(process.env.JWT_NAME!)?.value
-  console.log(token)
+
   const allowLoggedOut =
     request.nextUrl.pathname.startsWith('/login') ||
     request.nextUrl.pathname.startsWith('/signup')
+
   if (allowLoggedOut && token?.length) {
     return NextResponse.redirect(new URL('/', request.url))
   }
   if (allowLoggedOut) return
   if (!token?.length) {
+    console.log('redirecting')
     return NextResponse.redirect(new URL('/login', request.url))
   }
 }
 
 export const config = {
-  matcher: ['/((?!_next|api/auth).*)(.+)'],
+  matcher: ['/((?!_next|api/auth).*)(.+)', '/'],
 }
